9f4cd6c19918ab8e71549b2b969ce91596656a6f
Vladimir Filipev <vfilipyev@gmail.com>
2026-07-26 15:10:26 +0300

    fix: record packet migration snapshot

--- packages/db/drizzle/0016_packet_confirmation_evidence.sql
+++ packages/db/drizzle/0016_packet_confirmation_evidence.sql
@@ -6,10 +6,7 @@ BEGIN
 END;
 $$;
 --> statement-breakpoint
-ALTER TABLE "task_packets" ADD COLUMN "work_item_version" integer NOT NULL;
---> statement-breakpoint
+ALTER TABLE "agent_runs" ADD COLUMN "confirmed_packet_hash" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "task_packets" ADD COLUMN "work_item_version" integer NOT NULL;--> statement-breakpoint
+ALTER TABLE "agent_runs" ADD CONSTRAINT "agent_runs_confirmed_packet_hash_sha256" CHECK ("agent_runs"."confirmed_packet_hash" ~ '^[0-9a-f]{64}$');--> statement-breakpoint
 ALTER TABLE "task_packets" ADD CONSTRAINT "task_packets_work_item_version_positive" CHECK ("task_packets"."work_item_version" > 0);
---> statement-breakpoint
-ALTER TABLE "agent_runs" ADD COLUMN "confirmed_packet_hash" text NOT NULL;
---> statement-breakpoint
-ALTER TABLE "agent_runs" ADD CONSTRAINT "agent_runs_confirmed_packet_hash_sha256" CHECK ("agent_runs"."confirmed_packet_hash" ~ '^[0-9a-f]{64}$');
